3f33f2fdfdcd1692c479fdbd146081bb0f8b6ec2
Harshit Shukla <mrharshit2409@gmail.com>
2024-07-26 16:52:36 +0530

    added contact section and removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import TechStack from './components/Techstack'
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <TechStack />
+      <Contact />
     </>
   )
 }
